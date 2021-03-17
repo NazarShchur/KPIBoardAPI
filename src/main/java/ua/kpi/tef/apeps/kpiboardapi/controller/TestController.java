d4a653c64dad82c256bda013ef13d3a49b7cea4a
NazarShchur <51239588+NazarShchur@users.noreply.github.com>
2021-03-17 23:32:11 +0200

    swagger

--- src/main/java/ua/kpi/tef/apeps/kpiboardapi/controller/TestController.java
+++ src/main/java/ua/kpi/tef/apeps/kpiboardapi/controller/TestController.java
@@ -2,21 +2,28 @@ package ua.kpi.tef.apeps.kpiboardapi.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.kpi.tef.apeps.kpiboardapi.entity.User;
 import ua.kpi.tef.apeps.kpiboardapi.repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 @AllArgsConstructor
 public class TestController {
     private final UserRepository repository;
-    @GetMapping
-    public String test() {
+    @PostMapping
+    public User saveOrest() {
         User user = new User();
         user.setName("Orest");
-        repository.save(user);
-        return "Hello World";
+        return repository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getOrests() {
+        return repository.findAll();
     }
 }
